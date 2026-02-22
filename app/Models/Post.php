2d4d02b2c3e32ce900082f9f516283cb1e545673
Yuriy <yurabh1990@gmail.com>
2026-02-22 23:41:49 +0200

    Created Project added model Post

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'content',
+        'is_published',
+        'published_at',
+    ];
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
