dc4a0328b05da9530301af41f201a22dc297906a
Yuriy <yurabh1990@gmail.com>
2026-02-23 17:27:31 +0200

    Added models Category ,Post and performed changes in User added relationship to model and performed changes in migrations for current models

--- database/migrations/2026_02_22_185930_create_posts_table.php
+++ database/migrations/2026_02_22_185930_create_posts_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('content');
