6523c7c16b656122fde11ad987aa34d13092813d
Yuriy <yurabh1990@gmail.com>
2026-02-24 13:34:04 +0200

    Add model Comment and migration for it.

---
+++ app/Models/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'content',
+        'is_approved',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
