cbfe57400a99a5ca47f2743f2370600dccf5c125
Yuriy <yurabh1990@gmail.com>
2026-02-26 18:10:48 +0200

    Created two classes for validation request

---
+++ app/Http/Requests/Post/StorePostRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'category_id' => 'nullable|exists:categories,id',
+            'tags' => 'nullable|array',
+            'tags.*' => 'exists:tags,id',
+            'is_published' => 'sometimes|boolean',
+        ];
+    }
+}
