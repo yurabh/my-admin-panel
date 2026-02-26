cd01e394ff600865831510c1d6a150628a16649f
Yuriy <yurabh1990@gmail.com>
2026-02-26 18:09:14 +0200

    Small change ain model Tag

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @method static factory(int $int)
+ */
 class Tag extends Model
 {
+    use HasFactory;
 
     protected $fillable = [
         'name',
