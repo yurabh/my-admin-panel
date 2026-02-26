0fb50c5cb905387808147f83d7c279631cea14e7
Yuriy <yurabh1990@gmail.com>
2026-02-26 18:08:33 +0200

    Add resource for Post controller

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed $created_at
+ * @property mixed $updated_at
+ * @property mixed $user
+ * @property mixed $title
+ * @property mixed $slug
+ * @property mixed $content
+ * @property mixed $tags
+ */
+class PostResource extends JsonResource
+{
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+            'is_published' => $this->is_published,
+            'published_at' => $this->published_at,
+            'category' => $this->category ? [
+                'id' => $this->category->id,
+                'name' => $this->category->name,
+                'slug' => $this->category->slug,
+            ] : null,
+            'tags' => $this->tags->map(fn($tag) => [
+                'id' => $tag->id,
+                'name' => $tag->name,
+                'slug' => $tag->slug,
+            ]),
+            'author' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name,
+            ],
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
