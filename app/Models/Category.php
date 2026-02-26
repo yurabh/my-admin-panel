daaa3f8e29e04d1162338bfb546e7fe607f324c6
Yuriy <yurabh1990@gmail.com>
2026-02-26 18:04:33 +0200

    Added changes to the models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property \Illuminate\Support\HigherOrderCollectionProxy|mixed $id
+ * @method static factory(int $int)
+ */
 class Category extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        'name', 'slug'
+        'name', 'slug',
     ];
 
     public function posts()
