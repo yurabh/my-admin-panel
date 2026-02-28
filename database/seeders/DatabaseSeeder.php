ba28467a57186fbea566c052f18c9d56c9c37abe
Yuriy <yurabh1990@gmail.com>
2026-02-28 12:49:55 +0200

    Add PageSeeder for seeding data in db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             TagSeeder::class,
             PostSeeder::class,
+            PageSeeder::class,
         ]);
     }
 }
