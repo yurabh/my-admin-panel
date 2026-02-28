d33d187030455346ed2868a2e56784ce0a915eb4
Yuriy <yurabh1990@gmail.com>
2026-02-28 13:27:05 +0200

    Added routing for endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,16 @@
 <?php
+
+use App\Http\Controllers\Page\DeletePageController;
+use App\Http\Controllers\Page\ShowPageController;
+use App\Http\Controllers\Page\StorePageController;
+use App\Http\Controllers\Page\UpdatePageController;
+use App\Http\Controllers\Post\PostController;
+
+Route::prefix('admin')
+    ->group(function () {
+        Route::resource('posts', PostController::class);
+        Route::post('/pages', StorePageController::class);
+        Route::get('/pages/{page}', ShowPageController::class);
+        Route::put('/pages/{page}', UpdatePageController::class);
+        Route::delete('/pages/{page}', DeletePageController::class);
+    });
