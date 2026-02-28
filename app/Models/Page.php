1e4208fdffeb6bba63f71d202f608c741d151747
Yuriy <yurabh1990@gmail.com>
2026-02-28 13:06:01 +0200

    Fix in models

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Storage;
 
 class Page extends Model
 {
+
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'slug',
