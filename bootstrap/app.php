7d7ca0784d25e9f1eb7d0594e937278d27e608f1
Yuriy <yurabh1990@gmail.com>
2026-02-28 13:32:01 +0200

    Small fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,14 +6,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            // 'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        ]);
+        $middleware->api([
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
     })->create();
