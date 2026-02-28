0f75cf66119db7f2c4e831c482bbc816d102a1b6
Yuriy <yurabh1990@gmail.com>
2026-02-28 12:49:03 +0200

    Add PageFactory.php class create fake data for siding them in db

---
+++ database/factories/PageFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Page;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Page>
+ */
+class PageFactory extends Factory
+{
+    protected $model = Page::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->sentence(3);
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'content' => $this->faker->paragraphs(3, true),
+            'is_published' => $this->faker->boolean(70),
+            'user_id' => User::factory(),
+            'image' => null,
+        ];
+    }
+}
