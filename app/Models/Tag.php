aa623812599e8b1d77a4136e1f85dba3d3304b8b
Yuriy <yurabh1990@gmail.com>
2026-02-28 13:04:10 +0200

    Fix in models

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-/**
- * @method static factory(int $int)
- */
 class Tag extends Model
 {
     use HasFactory;
