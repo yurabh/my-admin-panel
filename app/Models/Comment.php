56e0c604da78f50119b64a117b6063283bea4bcd
Yuriy <yurabh1990@gmail.com>
2026-03-07 22:48:34 +0200

    Add Has factory trait

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @method static factory(int $int)
+ */
 class Comment extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'post_id',
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -10,9 +11,11 @@ use Illuminate\Database\Query\Builder;
 
 /**
  * @property mixed $user
+ * @method static factory()
  */
 class Post extends Model
 {
+    use HasFactory;
 
     protected $fillable = [
         'user_id',
