74255af6af7ca0f00b1561d807ee9250c6e9009b
Yuriy <yurabh1990@gmail.com>
2026-03-11 00:29:07 +0200

    Add middleware for checking is current user which try to enter resource has admin rule . Authorization case

---
+++ app/Http/Middleware/IsAdminMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\UserRole;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsAdminMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && auth()->user()->role === UserRole::ADMIN) {
+            return $next($request);
+        }
+        abort(403, Response::HTTP_FORBIDDEN);
+    }
+}
